ee264397dd7ea9271b586cf1d2dba4be1a843fbe
raibosome <raimi.bkarim@gmail.com>
2019-11-17 23:35:03 +0800

    v1.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import setup
 
 # Package meta-data.
-VERSION = "1.3.1"
+VERSION = "1.3.2"
 DESCRIPTION = "p2j: Convert Python scripts to Jupyter notebook with minimal intervention"
 
 # Import the README and use it as the long-description.
